39088e23e7da16faa0bf7bd35ba95f69bb1d13fe
web-dave <dave@webdave.de>
2017-06-15 09:25:10 +0200

    directive events and input

--- src/app/books/shared/order-btn.directive.ts
+++ src/app/books/shared/order-btn.directive.ts
@@ -1,20 +1,30 @@
-import { Directive, Input, ElementRef  } from '@angular/core';
+import { Directive, Input, ElementRef, OnChanges, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[orderBtn]'
 })
-export class OrderBtnDirective {
-
-  @Input()
-  set orderBtn(value: string) {
-    this.orderBtnElement.innerText = value;
-    this.orderBtnElement.onclick = () => {
-      console.log('buy me!!!')
-    }
-  }
+export class OrderBtnDirective implements OnChanges {
   orderBtnElement: HTMLButtonElement = document.createElement('button');
+
+  @Input() orderBtn: string;
+
+  ngOnChanges(changeObj) {
+    this.orderBtnElement.innerText = this.orderBtn;
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    console.log('mouseenter');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    console.log('mouseleave');
+  }
+
   constructor(elementRef: ElementRef) {
     elementRef.nativeElement.appendChild(this.orderBtnElement);
+    this.orderBtnElement.onclick = () => {
+      console.log('this.orderBtn:', this.orderBtn)
+    }
   }
 
 }
