9432008c38a61d55bfe660592bcf9de9d99ab7e5
David Muellerchen <dave@webdave.de>
2017-06-22 06:02:50 +0200

    fix strategy

--- src/app/shared/preload-delayed.service.ts
+++ src/app/shared/preload-delayed.service.ts
@@ -5,14 +5,15 @@ export class PreloadDelayedService implements PreloadingStrategy {
 
 
   preload(route: Route, fn: () => Observable<any>): Observable<any> {
-    console.log(route.data.preload);
 
-    if ((route.data !== undefined) && (route.data.preload)) {
-      return Observable.of(true).delay(3000).flatMap(() => fn());
-    }
-    if (route.data.preload) {
-      return fn();
-    }
+    if (route.data !== undefined) {
+      if (route.data.preload) {
+        return Observable.of(true).delay(3000).flatMap(() => fn());
+      }
 
+    } else {
+        return fn();
+    }
   }
+
 }
