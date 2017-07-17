3af3f54622ef690ed3cd10c1eda325e26b9e16c9
web-dave <dave@webdave.de>
2017-07-17 10:56:37 +0200

    Lint

--- src/app/books/book-details/book-details.component.spec.ts
+++ src/app/books/book-details/book-details.component.spec.ts
@@ -2,7 +2,7 @@ import { BooksService } from '../shared/books.service';
 import { BooksServiceStub } from './../shared/books.service.stub';
 import { RouterTestingModule } from '@angular/router/testing';
 import { PagesPipe } from './../shared/pages.pipe';
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookDetailsComponent } from './book-details.component';
