19e4541fc719d1367dbf51af1aa71eeb48845195
web-dave <dave@webdave.de>
2017-07-17 10:56:51 +0200

    Test BookEditComponent

--- src/app/books/book-edit/book-edit.component.spec.ts
+++ src/app/books/book-edit/book-edit.component.spec.ts
@@ -1,6 +1,18 @@
+import {BooksServiceStub} from '../shared/books.service.stub';
+import {BooksService} from '../shared/books.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookEditComponent } from './book-edit.component';
+
+@Component({
+  template: '',
+  selector: 'dummy'
+})
+class DummyComponent {
+}
 
 describe('BookEditComponent', () => {
   let component: BookEditComponent;
@@ -8,7 +20,12 @@ describe('BookEditComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BookEditComponent ]
+      declarations: [ BookEditComponent, DummyComponent ],
+      imports: [
+        RouterTestingModule.withRoutes([{ path: 'edit', component: DummyComponent }]),
+        FormsModule
+      ],
+      providers: [{ provide: BooksService, useClass: BooksServiceStub }]
     })
     .compileComponents();
   }));
