bcf55d2130fe2a5758bc1d5519fd16069557e8c6
web-dave <dave@webdave.de>
2017-07-17 10:57:05 +0200

    Test BookListComponent

--- src/app/books/book-list/book-list.component.spec.ts
+++ src/app/books/book-list/book-list.component.spec.ts
@@ -1,17 +1,36 @@
+import { RouterTestingModule } from '@angular/router/testing';
 import { BooksServiceStub } from './../shared/books.service.stub';
+import { BookPreviewComponent } from '../book-preview/book-preview.component';
+import { OrderBtnDirective } from '../shared/order-btn.directive';
+import { BooksService } from '../shared/books.service';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
 
 import { BookListComponent } from './book-list.component';
 
+@Component({
+  template: '',
+  selector: 'dummy'
+})
+class DummyComponent {
+}
 
-fdescribe('BookListComponent', () => {
+describe('BookListComponent', () => {
   let component: BookListComponent;
   let fixture: ComponentFixture<BookListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [BookListComponent],
-      providers: [BooksServiceStub]
+      declarations: [
+        BookListComponent,
+        BookPreviewComponent,
+        OrderBtnDirective,
+        DummyComponent
+      ],
+      imports: [
+        RouterTestingModule.withRoutes([{ path: 'edit', component: DummyComponent }])
+      ],
+      providers: [{ provide: BooksService, useClass: BooksServiceStub }]
     })
       .compileComponents();
   }));
