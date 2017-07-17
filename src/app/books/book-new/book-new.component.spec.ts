6a16f141504442762c3898db13873a4d1695117a
web-dave <dave@webdave.de>
2017-07-17 11:08:38 +0200

    Test BookNewComponent

--- src/app/books/book-new/book-new.component.spec.ts
+++ src/app/books/book-new/book-new.component.spec.ts
@@ -1,6 +1,18 @@
+import {BooksServiceStub} from '../shared/books.service.stub';
+import { Component } from '@angular/core';
+import {BooksService} from '../shared/books.service';
+import { RouterTestingModule } from '@angular/router/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { BookNewComponent } from './book-new.component';
+
+@Component({
+  template: '',
+  selector: 'dummy'
+})
+class DummyComponent {
+}
 
 describe('BookNewComponent', () => {
   let component: BookNewComponent;
@@ -8,7 +20,12 @@ describe('BookNewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BookNewComponent ]
+      declarations: [ BookNewComponent, DummyComponent ],
+      imports: [
+        RouterTestingModule.withRoutes([{ path: 'edit', component: DummyComponent }]),
+        ReactiveFormsModule
+      ],
+      providers: [{ provide: BooksService, useClass: BooksServiceStub }]
     })
     .compileComponents();
   }));
