ac8a07efb93260d42d3c2f5ec8121ad7ab233be2
web-dave <dave@webdave.de>
2017-07-17 10:42:58 +0200

    Test BookDetailsComponent

--- src/app/books/book-details/book-details.component.spec.ts
+++ src/app/books/book-details/book-details.component.spec.ts
@@ -1,6 +1,18 @@
+import { BooksService } from '../shared/books.service';
+import { BooksServiceStub } from './../shared/books.service.stub';
+import { RouterTestingModule } from '@angular/router/testing';
+import { PagesPipe } from './../shared/pages.pipe';
+import { Component } from '@angular/core'
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookDetailsComponent } from './book-details.component';
+
+@Component({
+  template: '',
+  selector: 'dummy'
+})
+class DummyComponent {
+}
 
 describe('BookDetailsComponent', () => {
   let component: BookDetailsComponent;
@@ -8,9 +20,17 @@ describe('BookDetailsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BookDetailsComponent ]
+      declarations: [
+        BookDetailsComponent,
+        PagesPipe,
+        DummyComponent
+      ],
+      imports: [
+        RouterTestingModule.withRoutes([{ path: 'edit', component: DummyComponent }])
+      ],
+      providers: [{ provide: BooksService, useClass: BooksServiceStub }]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
