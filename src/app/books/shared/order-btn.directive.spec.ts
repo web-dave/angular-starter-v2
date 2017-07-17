a5d735246627f2280332e98c1d2b7b63355228ad
web-dave <dave@webdave.de>
2017-07-17 11:28:55 +0200

    Test OrderBtnDirective

--- src/app/books/shared/order-btn.directive.spec.ts
+++ src/app/books/shared/order-btn.directive.spec.ts
@@ -1,8 +1,45 @@
 import { OrderBtnDirective } from './order-btn.directive';
 
-// describe('OrderBtnDirective', () => {
-//   it('should create an instance', () => {
-//     const directive = new OrderBtnDirective();
-//     expect(directive).toBeTruthy();
-//   });
-// });
+import { Component, DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  selector: 'mock-component',
+  template: '<div orderBtn="Kauf Mich!"></div>',
+})
+class MockComponent {
+}
+
+describe('A component with OrderBtnDirective', () => {
+  let fixture: ComponentFixture<MockComponent>;
+  let element: DebugElement;
+  let sut: OrderBtnDirective;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [MockComponent, OrderBtnDirective],
+    });
+
+    fixture = TestBed.createComponent(MockComponent);
+    element = fixture.debugElement.query(By.directive(OrderBtnDirective));
+    sut = element.injector.get(OrderBtnDirective);
+  });
+
+  fdescribe('orderBtn', () => {
+    it('should be a button', () => {
+      expect(sut.orderBtnElement.tagName).toBe('BUTTON');
+    });
+
+    it('should show `Kauf mich`', () => {
+      fixture.detectChanges();
+      expect(sut.orderBtnElement.innerText).toBe('Kauf Mich!');
+    });
+
+    it('should write a console.log if cklicked', () => {
+      spyOn(console, 'log');
+      sut.orderBtnElement.click();
+      expect(console.log).toHaveBeenCalled();
+    });
+  });
+});
