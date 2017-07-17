199f48a3a2276e53b728171584f3fcf0cce9fc9e
web-dave <dave@webdave.de>
2017-07-17 11:09:04 +0200

    Test MyNavComponent

--- src/app/my-nav/my-nav.component.spec.ts
+++ src/app/my-nav/my-nav.component.spec.ts
@@ -1,14 +1,24 @@
+import { RouterTestingModule } from '@angular/router/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component } from '@angular/core';
 import { MyNavComponent } from './my-nav.component';
 
+@Component({
+  template: '',
+  selector: 'dummy'
+})
+class DummyComponent {
+}
 describe('MyNavComponent', () => {
   let component: MyNavComponent;
   let fixture: ComponentFixture<MyNavComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MyNavComponent ]
+      declarations: [ MyNavComponent, DummyComponent ],
+      imports: [
+        RouterTestingModule.withRoutes([{ path: 'edit', component: DummyComponent }])
+      ],
     })
     .compileComponents();
   }));
