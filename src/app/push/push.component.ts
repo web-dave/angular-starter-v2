b646d5a8986cea3b10ff88f73cffe15af856c751
web-dave <dave@webdave.de>
2017-08-02 08:43:18 +0200

    remove indexDB and use localStorage

--- src/app/push/push.component.ts
+++ src/app/push/push.component.ts
@@ -1,7 +1,6 @@
 import { PushService } from '../shared/push.service';
 import { Component, OnInit } from '@angular/core';
 import { NgServiceWorker, NgPushRegistration } from '@angular/service-worker';
-import { IdbKeyval } from '../../ngsw/plugins/indexdbhelper';
 
 @Component({
   selector: 'push',
@@ -11,12 +10,10 @@ import { IdbKeyval } from '../../ngsw/plugins/indexdbhelper';
 export class PushComponent implements OnInit {
   pubKey = 'BGPqIbjDKI5Qja2gHX971n1ZoDL14hb-FihSMDFfvnRjLEl7XQ6FFOYGHxjz9ESB3HX5R10yW27KXARbDrDWfCc';
   msg = { title: '', message: '' };
-  idbKeyval: IdbKeyval;
 
   constructor(private sw: NgServiceWorker, private push: PushService) { }
 
   ngOnInit() {
-    this.idbKeyval = new IdbKeyval();
   }
 
   subscribeToPush() {
@@ -35,34 +32,34 @@ export class PushComponent implements OnInit {
     this.push.unsubscribeFromPush();
   }
 
-  sendMessage(msg) {
-
-    if ('SyncManager' in window) {
-
-      this.idbKeyval.set('send-push', msg)
-        .then(function () {
-          console.log('[SW Registration]: Wrote data to IndexedDB', msg)
-
-          navigator.serviceWorker.ready.then(function (swRegistration) {
-            swRegistration.sync.register('send-push')
-              .then(function () {
-                console.log('[SW Registration]: Background sync registered')
-              })
-              .catch(function (error) {
-                // system was unable to register for a sync,
-                // this could be an OS-level restriction
-                console.log('[SW Registration]: Error registering Background sync', error)
-              })
-          })
-        })
-
-    console.log('[SW Registration]: Background sync initiated')
-  } else {
-    console.log('[SW Registration]: Background sync isn`t supported in this browser')
-  }
+  sendPush(msg) {
     this.push.sendPush(msg).subscribe(data => {
       msg = { title: '', message: '' };
+      window.localStorage.removeItem('send-push');
     })
+  }
+
+  sendMessage(msg) {
+    if ('SyncManager' in window) {
+
+      navigator.serviceWorker.ready.then(function (swRegistration) {
+        swRegistration.sync.register('send-push')
+          .then(function () {
+            window.localStorage.setItem('send-push', msg);
+            console.log('[SW Registration]: Background sync registered')
+          })
+          .catch(function (error) {
+            // system was unable to register for a sync,
+            // this could be an OS-level restriction
+            console.log('[SW Registration]: Error registering Background sync', error)
+          })
+      })
+      this.sendPush(msg);
+      console.log('[SW Registration]: Background sync initiated')
+    } else {
+      console.log('[SW Registration]: Background sync isn`t supported in this browser')
+      this.sendPush(msg);
+    }
   }
 
 }
